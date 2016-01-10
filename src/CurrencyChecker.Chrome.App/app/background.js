8928dc5a2a3209b531c8944558c7200d659aa3d3
Michal <michal.maminski@gmail.com>
2016-01-09 19:06:39 -0800

    Added ticker - currently displaying only some dumb string

--- src/CurrencyChecker.Chrome.App/app/background.js
+++ src/CurrencyChecker.Chrome.App/app/background.js
@@ -4,15 +4,21 @@ var defaultSettings = {
     alarmName: "periodAlarm"
 };
 
-var currencyDataExchanger;
-currencyDataExchanger = (function ($) {
+var canvasElement = document.createElement("canvas");
+canvasElement.width = 19;
+canvasElement.height = 19;
+
+var currencyDataExchanger = (function ($) {
 
     var self = {};
-
+    var counter = 1;
     function setBadgeValue(value) {
-        chrome.browserAction.setBadgeText({
-            text: value
-        });
+        //chrome.browserAction.setBadgeText({
+        //    text: value
+        //});
+
+        self.ticker.redraw();
+
     }
 
     function gotDataFromServer(reponseFromServer) {
@@ -59,7 +65,8 @@ currencyDataExchanger = (function ($) {
         createDelayedAction(this.settings);
     };
 
-    self.init = function (defaultSettings) {
+    self.init = function (defaultSettings, ticker) {
+        this.ticker = ticker;
         this.settings = defaultSettings;
         chrome.alarms.onAlarm.addListener(periodicEventHandler);
         createDelayedAction(defaultSettings);
@@ -71,5 +78,82 @@ currencyDataExchanger = (function ($) {
     return self;
 })($);
 
-currencyDataExchanger.init(defaultSettings);
+var iconTicker = (function (canvasElement) {
+    var canvasHeight = 19;
+    var canvasWidth = 19;
+    var canvas = canvasElement;
+    var firstTextPosition = canvasWidth;
+    var secondTextPosition = canvasWidth;
+    var currencyTextWidth = 38;
+    var textShiftStep = 1;
+    var self = this;
+    var canPrintFirst = true;
+    var canPrintSecond = false;
 
+    var spaceBetweenTicks = 6;
+
+    function redrawIcon(value) {
+        var context = canvas.getContext('2d');
+        context.clearRect(0, 0, canvasWidth, canvasHeight);
+        context.fillStyle = "#262626";
+        context.fillRect(0, 0, 19, 19);
+
+        context.textAlign = "left";
+        context.textBaseline = "top";
+        //chrome.browserAction.setBadgeBackgroundColor({ color: "green" });
+        chrome.browserAction.setBadgeText({ text: "SELL" });
+
+
+        context.fillStyle = "white";
+        context.font = "13px Arial";
+
+        if (canPrintFirst) {
+            context.fillText("4,1234", firstTextPosition, -1);
+            firstTextPosition -= textShiftStep;
+        }
+
+        if (firstTextPosition < ((canvasWidth - spaceBetweenTicks) - currencyTextWidth)) {
+            canPrintSecond = true;
+        }
+
+        if (firstTextPosition < 0 - currencyTextWidth) {
+            firstTextPosition = canvasWidth;
+            canPrintFirst = false;
+        }
+
+        if (canPrintSecond) {
+            context.fillText("4,1234", secondTextPosition, -1);
+            secondTextPosition -= textShiftStep;
+        }
+
+        if (secondTextPosition < ((canvasWidth - spaceBetweenTicks) - currencyTextWidth)) {
+            canPrintFirst = true;
+        }
+
+        if (secondTextPosition < 0 - currencyTextWidth) {
+            secondTextPosition = canvasWidth;
+            canPrintSecond = false;
+        }
+
+        chrome.browserAction.setIcon({
+            imageData: context.getImageData(0, 0, 19, 19)
+        });
+    };
+
+    function init(currencyDataExchanger) {
+        self.currencyDataExchanger = currencyDataExchanger;
+        setInterval(redrawIcon, 80);
+
+
+    };
+
+    return {
+        redraw: redrawIcon,
+        init: init
+    };
+})(canvasElement);
+
+
+
+currencyDataExchanger.init(defaultSettings, iconTicker);
+iconTicker.init(currencyDataExchanger);
